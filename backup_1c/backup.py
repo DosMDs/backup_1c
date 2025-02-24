36fff4703ff67a336e28663749c6b0bebd8cabed
Oleg Skakunov <dosmds@gmail.com>
2025-02-24 20:38:49 +0300

    Добавил обновление статуса при его повторной выгрузке

--- backup_1c/backup.py
+++ backup_1c/backup.py
@@ -3,7 +3,13 @@
 import logging
 from typing import List
 
-from backup_1c.db_utils import add_file, get_all_database_creds
+from backup_1c.config.database import FileStatus
+from backup_1c.db_utils import (
+    add_file,
+    get_all_database_creds,
+    get_file_by_path,
+    update_file_status,
+)
 from backup_1c.models import DatabaseCreds
 from backup_1c.utils import run_ibcmd
 
@@ -17,7 +23,11 @@ def backup_1c() -> None:
         logger.info(f"Выгрузка базы {creds.db_name}")
         full_path = run_ibcmd(creds.db_name, creds.username, creds.password)
         if full_path:
-            add_file(full_path)
+            file = get_file_by_path(full_path)
+            if file:
+                update_file_status(file.id, FileStatus.NEW)
+            else:
+                add_file(full_path)
             logger.info(f"Выгрузка базы {creds.db_name} завершена")
         else:
             logger.error(f"База {creds.db_name} не выгружена")
