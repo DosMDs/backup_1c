facceabed22886db8791661633cba833a7b680f2
Oleg Skakunov <dosmds@gmail.com>
2025-02-24 18:55:37 +0300

    Тестирование изменения статуса файла

--- tests/test_db_utils.py
+++ tests/test_db_utils.py
@@ -2,13 +2,21 @@
 
 from unittest.mock import MagicMock
 
-from backup_1c.db_utils import add_file
+import pytest
+
+from backup_1c.db_utils import add_file, update_file_status
 
 
-def test_add_file(monkeypatch):
+def test_add_file(monkeypatch) -> None:
     """Тестирование добавления файла в базу."""
     db_mock = MagicMock()
     monkeypatch.setattr("backup_1c.db_utils.get_db", lambda: iter([db_mock]))
     result = add_file("/path/to/file.dt")
     assert result.full_path == "/path/to/file.dt"
     db_mock.commit.assert_called_once()
+
+
+def test_update_file_status_invalid_type():
+    """Тестирование изменения статуса файла."""
+    with pytest.raises(ValueError, match="Статус должен быть из FileStatus"):
+        update_file_status(1, "invalid_status")
