22b03ccd541dd7e4e1bcccd3f223a472841a6c2f
Oleg Skakunov <dosmds@gmail.com>
2025-02-24 18:36:15 +0300

    изменил тестирование config.py

--- backup_1c/config/config.py
+++ backup_1c/config/config.py
@@ -10,21 +10,35 @@ load_dotenv()
 class Config:
     """Хранение переменных окружения."""
 
-    LOG_PATH = os.getenv("LOG_PATH", "logs/app.log")
-    TEMP_PATH = os.getenv("TEMP_PATH", "tmp")
-    DB_PATH = os.getenv("DB_PATH", "app.sqlite3")
-    DATABASE_URL = f"sqlite:///{DB_PATH}"
-    BACKUP_PATH = os.getenv("BACKUP_PATH", "/backup")
-    # Данные для ibcmd
-    ENTERPRISE_PATH = os.getenv("ENTERPRISE_PATH", "/opt/1cv8/x86_64/")
-    ENTERPRISE_VERSION = os.getenv("ENTERPRISE_VERSION")
-    DB_SERVER = os.getenv("DB_SERVER", "localhost")
-    DBMS = os.getenv("DMBS", "PostgreSQL")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASS")
+    # LOG_PATH = os.getenv("LOG_PATH", "logs/app.log")
+    # TEMP_PATH = os.getenv("TEMP_PATH", "tmp")
+    # DB_PATH = os.getenv("DB_PATH", "app.sqlite3")
+    # DATABASE_URL = f"sqlite:///{DB_PATH}"
+    # BACKUP_PATH = os.getenv("BACKUP_PATH", "/backup")
+    # # Данные для ibcmd
+    # ENTERPRISE_PATH = os.getenv("ENTERPRISE_PATH", "/opt/1cv8/x86_64/")
+    # ENTERPRISE_VERSION = os.getenv("ENTERPRISE_VERSION")
+    # DB_SERVER = os.getenv("DB_SERVER", "localhost")
+    # DBMS = os.getenv("DMBS", "PostgreSQL")
+    # DB_USER = os.getenv("DB_USER")
+    # DB_PASS = os.getenv("DB_PASS")
 
     def __init__(self) -> None:
         """Инициализация класса Config."""
+        self.LOG_PATH = os.getenv("LOG_PATH", "logs/app.log")
+        self.TEMP_PATH = os.getenv("TEMP_PATH", "tmp")
+        self.DB_PATH = os.getenv("DB_PATH", "app.sqlite3")
+        self.DATABASE_URL = f"sqlite:///{self.DB_PATH}"
+        self.BACKUP_PATH = os.getenv("BACKUP_PATH", "/backup")
+        self.ENTERPRISE_PATH = os.getenv(
+            "ENTERPRISE_PATH", "/opt/1cv8/x86_64/"
+        )
+        self.ENTERPRISE_VERSION = os.getenv("ENTERPRISE_VERSION")
+        self.DB_SERVER = os.getenv("DB_SERVER", "localhost")
+        self.DBMS = os.getenv("DMBS", "PostgreSQL")
+        self.DB_USER = os.getenv("DB_USER")
+        self.DB_PASS = os.getenv("DB_PASS")
+
         if not self.DB_USER or not self.DB_PASS:
             raise ValueError("DB_USER или DB_PASS не установлены")
 
--- tests/test_config.py
+++ tests/test_config.py
@@ -4,8 +4,24 @@ import os
 from unittest.mock import patch
 
 import pytest
-
 from backup_1c.config.config import Config
+
+
+@patch.dict(os.environ, {"DB_USER": "test_user", "DB_PASS": "test_pass"})
+def test_config_init():
+    """Тест инициализации класса Config."""
+    config = Config()
+    assert config.LOG_PATH == "logs/app.log"
+    assert config.TEMP_PATH == "tmp"
+    assert config.DB_PATH == "app.sqlite3"
+    assert config.DATABASE_URL == "sqlite:///app.sqlite3"
+    assert config.BACKUP_PATH == "/backup"
+    assert config.ENTERPRISE_PATH == "/opt/1cv8/x86_64/"
+    assert config.ENTERPRISE_VERSION is None
+    assert config.DB_SERVER == "localhost"
+    assert config.DBMS == "PostgreSQL"
+    assert config.DB_USER == "test_user"
+    assert config.DB_PASS == "test_pass"
 
 
 @patch.dict(os.environ, {"DB_USER": "", "DB_PASS": ""})
